32bf7f1a04f7415481bbbf590e5f13a261940a70
Jason Sweet <hoozifachi@gmail.com>
2015-12-14 14:24:20 -0800

    Add chapter 4 example 8

---
+++ chapter04/7-examples/test.js
@@ -0,0 +1,34 @@
+describe('Nav Bar', function() {
+  var injector;
+  var element;
+  var scope;
+  var compiler;
+  var httpBackend;
+
+  beforeEach(function() {
+    injector = angular.injector(['myApp', 'ngMoclE2E']);
+    intercepts = {};
+
+    injector.invoke(function($rootScope, $compile, $httpBackend) {
+      scope = $rootScope.$new();
+      compiler = $compile;
+      httpBackend = $httpBackend;
+    });
+  });
+
+  it('shows logged in users name', function(done) {
+    httpBackend.whenGET('/7-examples/template.html').passThrough();
+    httpBackend.expectGET('/api/v1/me').respond({
+      user: { profile: { username: 'John' } }
+    });
+
+    element = compiler('<user-menu></user-menu>')(scope);
+
+    scope.$on('MyHttpController', function() {
+      httpBackend.flush();
+      assert.notEqual(element.find('.user').css('display'), 'none');
+      assert.equal(element.find('.user').text().trim(), 'Current User: John');
+      done();
+    });
+  });
+});
